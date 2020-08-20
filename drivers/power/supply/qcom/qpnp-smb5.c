6200ff7ccf62739d2436265b6411a7cccfadaeaf
Demon000 <demonsingur@gmail.com>
2020-08-20 14:50:23 +0300

    Revert "power: qcom: smb5: set OTG to 1A"
    This reverts commit 520b695ff380ed73108afd32512746319f89473f.

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -459,7 +459,9 @@ static int smb5_parse_dt(struct smb5 *chip)
 	rc = of_property_read_u32(node,
 				"qcom,otg-cl-ua", &chg->otg_cl_ua);
 	if (rc < 0)
-		chg->otg_cl_ua = MICRO_1PA;
+		chg->otg_cl_ua =
+			(chip->chg.chg_param.smb_version == PMI632_SUBTYPE)
+						? MICRO_1PA : MICRO_3PA;
 
 	rc = of_property_read_u32(node, "qcom,chg-term-src",
 			&chip->dt.term_current_src);
